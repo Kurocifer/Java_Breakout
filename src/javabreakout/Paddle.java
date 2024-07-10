a20b0defc2b9c797c01378b05d9669fd098ee5b1
Cifer-ti <draculemihawk232@gmail.com>
2024-07-10 08:48:25 +0100

    Add the paddle class

---
+++ src/javabreakout/Paddle.java
@@ -0,0 +1,47 @@
+package javabreakout;
+
+public class Paddle {
+    private int paddleX;
+    private int paddleY;
+    private int paddleHeight;
+    private int paddleWidth;
+    private int paddleSpeed;
+
+    public Paddle(int paddleX, int paddleY, int paddleWidth,
+                  int paddleHeight, int paddleSpeed) {
+
+        this.paddleX = paddleX;
+        this.paddleY = paddleY;
+        this.paddleWidth = paddleWidth;
+        this.paddleHeight = paddleHeight;
+        this.paddleSpeed = paddleSpeed;
+    }
+
+    public void movingLeft() {
+        paddleX -= paddleSpeed;
+    }
+
+    public void movingRight() {
+        paddleX += paddleSpeed;
+    }
+
+    public int getPaddleX() {
+        return paddleX;
+    }
+
+    public int getPaddleY() {
+        return paddleY;
+    }
+
+    public int getPaddleHeight() {
+        return paddleHeight;
+    }
+
+    public int getPaddleWidth() {
+        return paddleWidth;
+    }
+
+    public int getPaddleSpeed() {
+        return paddleSpeed;
+    }
+}
